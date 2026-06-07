74bcc57f2e40a80b0149da1933861c577790fe06
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-06-08 08:24:25 +0900

    fix: allocate memory multiplied by number of channels

--- src/track/audio_track/track_impl/process.rs
+++ src/track/audio_track/track_impl/process.rs
@@ -4,6 +4,6 @@ impl AudioTrack {
     // --- LOCAL BUFFER ---
 
     pub(super) fn init_local_buffers(&mut self) {
-        self.local_buffer = vec![0.0; self.audio_ctx.buffer_size];
+        self.local_buffer = vec![0.0; self.audio_ctx.buffer_size * self.audio_ctx.channels];
     }
 }
